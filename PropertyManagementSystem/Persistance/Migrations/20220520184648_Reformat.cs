f5c78bc4496a1a260f36d34e5fff52603999808c
SamMarrello <sammarrello@gmail.com>
2022-05-24 11:39:33 -0400

    trying to fix db connections

---
+++ PropertyManagementSystem/Persistance/Migrations/20220520184648_Reformat.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Persistance.Migrations
+{
+    public partial class Reformat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Rooms",
+                table: "Rooms");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Guests",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Guests");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomId",
+                table: "Rooms",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddColumn<int>(
+                name: "GuestId",
+                table: "Guests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Rooms",
+                table: "Rooms",
+                column: "RoomId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Guests",
+                table: "Guests",
+                column: "GuestId");
+
+            migrationBuilder.CreateTable(
+                name: "Stays",
+                columns: table => new
+                {
+                    StayId = table.Column<Guid>(type: "uuid", nullable: false),
+                    StartDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    EndDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    RoomId = table.Column<int>(type: "integer", nullable: true),
+                    GuestId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stays", x => x.StayId);
+                    table.ForeignKey(
+                        name: "FK_Stays_Guests_GuestId",
+                        column: x => x.GuestId,
+                        principalTable: "Guests",
+                        principalColumn: "GuestId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Stays_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "RoomId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stays_GuestId",
+                table: "Stays",
+                column: "GuestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stays_RoomId",
+                table: "Stays",
+                column: "RoomId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stays");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Rooms",
+                table: "Rooms");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Guests",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "RoomId",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "GuestId",
+                table: "Guests");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Rooms",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "Id",
+                table: "Guests",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Rooms",
+                table: "Rooms",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Guests",
+                table: "Guests",
+                column: "Id");
+        }
+    }
+}
